645a25b24377a2ddd6dac403288d2f3e6a26506e
0xd3d5ec <abdelaalatif253@gmail.com>
2023-06-07 13:55:24 +0200

    Update arch_custom.sh

--- arch_custom.sh
+++ arch_custom.sh
@@ -19,7 +19,13 @@ paru -S --noconfirm papirus-icon-theme
 # 4. Change xfce4 icons to Papirus
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
 
-# 5. Change xfce4 panel or import panel profile
+# 5. Install blackarch-config-zsh from BlackArch repo
+pacman -S --noconfirm blackarch-config-zsh
+
+# 6. Copy zsh config file
+cp /usr/share/blackarch-config-zsh/zshrc /etc/zsh/zshrc
+
+# 7. Change xfce4 panel or import panel profile
 # Option 1: Change xfce4 panel
 # xfconf-query -c xfce4-panel -p /panels/panel-1/style -s "vertical"
 # xfconf-query -c xfce4-panel -p /panels/panel-1/size -s "32"
@@ -30,7 +36,7 @@ xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
 # Option 2: Import panel profile (if available)
 xfce4-panel-profiles -R ~/dotfiles/panel_profiles/panel_profile.tar.bz2
 
-# 6. Install other important tools for VM optimization
+# 8. Install other important tools for VM optimization
 pacman -S --noconfirm xfce4-power-manager tlp
 
 echo "XFCE4 customization and VM optimization completed successfully!"
