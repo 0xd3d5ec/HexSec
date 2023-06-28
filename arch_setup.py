ab1ad934342a62e0e3417e07853b28eeff272c0d
0xd3d5ec <abdelaalatif253@gmail.com>
2023-06-28 17:55:08 +0200

    Create arch_setup.py

---
+++ arch_setup.py
@@ -0,0 +1,68 @@
+import os
+
+# Set up environment variables
+os.environ['TZ'] = 'UTC'
+os.system('timedatectl set-ntp true')
+
+# Partition the disk
+os.system('cfdisk')
+
+# Format partitions
+os.system('mkfs.btrfs /dev/sdaX')  # Replace 'sdaX' with your partition
+
+# Mount the root partition
+os.system('mount /dev/sdaX /mnt')  # Replace 'sdaX' with your partition
+
+# Install the base system
+os.system('pacstrap /mnt base base-devel linux-lts linux-firmware')
+
+# Generate fstab
+os.system('genfstab -U /mnt >> /mnt/etc/fstab')
+
+# Chroot into the new system
+os.system('arch-chroot /mnt')
+
+# Set the system clock
+os.system('ln -sf /usr/share/zoneinfo/Region/City /etc/localtime')  # Replace 'Region/City' with your timezone
+os.system('hwclock --systohc')
+
+# Configure locales
+os.system('echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen')
+os.system('locale-gen')
+os.system('echo "LANG=en_US.UTF-8" > /etc/locale.conf')
+
+# Configure hostname
+os.system('echo "myhostname" > /etc/hostname')  # Replace 'myhostname' with your desired hostname
+
+# Set the root password
+os.system('passwd')
+
+# Install and configure GRUB
+os.system('pacman -S grub')
+os.system('grub-install --target=i386-pc /dev/sda')  # Replace 'sda' with your disk
+os.system('grub-mkconfig -o /boot/grub/grub.cfg')
+
+# Install and configure XFCE4
+os.system('pacman -S xfce4 xfce4-goodies')
+
+# Install Snapper and Btrfs-progs
+os.system('pacman -S snapper btrfs-progs')
+
+# Configure Btrfs
+os.system('snapper -c root create-config /')
+os.system('systemctl enable snapper-timeline.timer')
+os.system('systemctl enable snapper-cleanup.timer')
+
+# Configure Snapper to display snapshots in GRUB
+os.system('sed -i "s/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=countdown/g" /etc/default/grub')
+os.system('sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=10/g" /etc/default/grub')
+os.system('grub-mkconfig -o /boot/grub/grub.cfg')
+
+# Exit the chroot environment
+os.system('exit')
+
+# Unmount partitions
+os.system('umount -R /mnt')
+
+# Reboot the system
+os.system('reboot')
